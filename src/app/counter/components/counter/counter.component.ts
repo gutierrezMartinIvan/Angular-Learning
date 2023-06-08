50ad0d478636eea851f754ddaa386f2c02c97188
Yural <martin192012@gmail.com>
2023-06-07 22:14:35 -0300

    first commit on angular bases

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-counter',
+    template: `
+    <h3>Counter: {{counter}}</h3>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="decreaseBy(1)">-1</button>
+    `
+})
+export class CounterComponent {
+    public counter: number = 10;
+
+    increaseBy = (value: number) => { this.counter += value; };
+
+    decreaseBy = (value: number) => { this.counter -= value; };
+
+    resetCounter = () => { this.counter = 10; };
+}
---
+++ src/app/heroes/components/hero/hero.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+    public name: string = 'ironman';
+    public age: number = 45;
+
+    get capitalizedName(): string {
+        return this.name.toUpperCase();
+    }
+
+    getHeroDescription = (): string => { return `${ this.name } - ${this.age }`; };
+
+    changeHero = ( hero: string ) => { this.name = hero; };
+
+    changeAge = ( age: number ) => { this.age = age; };
+
+    resetForm = () => {
+        this.name = 'ironman';
+        this.age = 45;
+    }
+}
